c7bbf31d5ded4c1482810813370dbaea1b6508d2
ethbergMuzola <ethbergmuzola500@gmail.com>
2024-07-16 22:50:35 +0200

    Feat: Add touch event for mobile phone

--- scripts.js
+++ scripts.js
@@ -27,6 +27,12 @@ function displayImages() {
                tile.addEventListener("dragleave",dragLeave);
                tile.addEventListener("drop",dragDrop);
                tile.addEventListener("dragend",dragEnd);
+
+            
+               tile.addEventListener("touchstart", touchStart);
+               tile.addEventListener("touchmove", touchMove);
+               tile.addEventListener("touchend", touchEnd);
+
                document.getElementById("board").append(tile);
             }
         } 
@@ -46,37 +52,57 @@ function displayImages() {
         otherTile=this
           
     }
+
+    function touchStart(e) {
+        currTile = e.target;
+    }
+
+    function touchMove(e) {
+        e.preventDefault();
+        var touch = e.touches[0];
+        var element = document.elementFromPoint(touch.clientX, touch.clientY);
+        if (element && element.tagName === 'IMG') {
+            otherTile = element;
+        }
+    }
+
+    function touchEnd() {
+        swapTiles();
+    }
     function dragEnd(){
-        if (!otherTile.src.includes("3.jpg")) {
+        swapTiles();
+    }
+
+    function swapTiles() {
+        if (!otherTile || !otherTile.src.includes("3.jpg")) {
             return;
         }
-    
-        let currCoords=currTile.id.split('-');
-        let r= parseInt(currCoords[0]);
-        let c= parseInt(currCoords[1]);
-        console.log(c,r,currCoords)
-    
-        let otherCoords=otherTile.id.split('-');
-      
-    
-        let r2=parseInt(otherCoords[0]);
-        let c2=parseInt(otherCoords[1]);
-        console.log("ded",r2,c2);
-        let moveLeft=r==r2 && c2==c-1;
-        let moveRight=r==r2 && c2==c+1;
-        let moveUp=c== c2 && r2== r-1;
-        let moveDown= c== c2 && r2 == r+1;
-    
-        let isAdjacent= moveLeft|| moveRight|| moveUp||moveDown
-        if(isAdjacent){
-            var currImg=currTile.src;
-            var otherImg=otherTile.src;
-            currTile.src=otherImg;
-            otherTile.src =currImg;
+
+        let currCoords = currTile.id.split('-');
+        let r = parseInt(currCoords[0]);
+        let c = parseInt(currCoords[1]);
+
+        let otherCoords = otherTile.id.split('-');
+        let r2 = parseInt(otherCoords[0]);
+        let c2 = parseInt(otherCoords[1]);
+
+        let moveLeft = r == r2 && c2 == c - 1;
+        let moveRight = r == r2 && c2 == c + 1;
+        let moveUp = c == c2 && r2 == r - 1;
+        let moveDown = c == c2 && r2 == r + 1;
+
+        let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
+        if (isAdjacent) {
+            var currImg = currTile.src;
+            var otherImg = otherTile.src;
+            currTile.src = otherImg;
+            otherTile.src = currImg;
             turns += 1;
             document.getElementById("turns").innerText = turns;
-        }     
-    }    
+        }
+    }
+
+
 }
 
 function shuffleImages(arr) {
